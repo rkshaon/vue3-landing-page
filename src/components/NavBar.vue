74ea9816b91c8b816a73a9a371f4e768528e4ee3
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-24 12:47:10 +0600

    forgot

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,7 +11,7 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">About</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
---
+++ src/routers.js
@@ -0,0 +1,2 @@
+import { createWebHistory, createRouter } from 'vue-router'
+import NavBar from './components/NavBar.vue'
